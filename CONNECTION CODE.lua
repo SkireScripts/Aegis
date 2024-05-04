a388378c7cebc1a3637eaed13440508c38187b33
iiprixe? <167163979+SkireScripts@users.noreply.github.com>
2024-05-04 10:32:33 -0400

    Update CONNECTION CODE.lua

--- CONNECTION CODE.lua
+++ CONNECTION CODE.lua
@@ -2,7 +2,7 @@ getgenv().api = "https://yourapi/"
 while true do wait(.1)
     local code = game:HttpGet(api.."read")
     if code ~= "" then
-        runcode(code)
+        loadstring(code)() -- or runcode(code) / executecode(code)
         local clear = game:HttpGet(api.."clear")
     end
 end
